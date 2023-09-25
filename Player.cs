e796417f4679c479170a716a9e8e96387033bfff
Thomas Brown <github@honourity.com>
2023-09-25 21:13:36 +1000

    added a screen flash on spacebar (early gunshot implementation)

--- Player.cs
+++ Player.cs
@@ -1,28 +1,34 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 3f;
-	
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	const float Speed = 5.0f;
+	float _gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	ShaderMaterial _screenFlash;
+
+	public override void _Ready()
+	{
+		_screenFlash = GetParentNode3D().GetNode<CanvasLayer>("CanvasLayer").GetNode<ColorRect>("ScreenFlash").Material as ShaderMaterial;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector3 velocity = Velocity;
+		var velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor()) velocity.Y -= gravity * (float)delta;
+		// Apply gravity
+		if (!IsOnFloor()) velocity.Y -= _gravity * (float)delta;
 
-		// Handle Jump.
-		//if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) velocity.Y = JumpVelocity;
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			DoShoot();
+		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDirection = Input.GetVector("ui_home", "ui_end", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDirection.X, 0, inputDirection.Y)).Normalized();
+		var inputDirection = Input.GetVector("ui_home", "ui_end", "ui_up", "ui_down");
+		var direction = (Transform.Basis * new Vector3(inputDirection.X, 0, inputDirection.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -46,5 +52,16 @@ public partial class Player : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	void DoShoot()
+	{
+		_screenFlash.SetShaderParameter("toggle", 1.0);
+		
+		var timer = GetTree().CreateTimer(0.03);
+		timer.Timeout += () => { _screenFlash.SetShaderParameter("toggle", 0.0); };
+		
+		//todo - do a raycast to determind what is hit and where
+		//todo - place a decal if it hits gridmap stuff (which auto removes itself after x seconds)
 	}
 }
